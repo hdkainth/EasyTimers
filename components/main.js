85c18295da9c1b45f81dded9a387748932324f66
Gurdeep Singh <deephome@gmail.com>
2023-07-08 22:01:12 -0700

    Create main style sheet file Replaced hard coded timeseq constructor with updates from main Start using autotext

--- components/main.js
+++ components/main.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
+import { AutoSizeText, ResizeTextMode } from 'react-native-auto-size-text';
 import {View, FlatList, Text, Image, TouchableOpacity} from "react-native";
 import Header from './Header';
 import AddTimer from './add_timer';
 import Timer from "../timer";
 import TimerList from "../timer_list";
+import { mainStyle, timerSeqListStyle } from '../css/main';
 
 class Main extends Component {
   constructor(props) {
@@ -18,10 +20,26 @@ class Main extends Component {
     this.handleSequenceEditRef = this.handleSequenceEdit.bind(this)
 
     this.sequenceList = [
-      { name: "Sequence Number ", timer: new TimerList(), key: 1 },
-      { name: "Sequence Number ", timer: new TimerList(), key: 2 },
-      { name: "Sequence Number ", timer: new TimerList(), key: 3 }
+      { name: "Short strenching ", timer: new TimerList(), key: 1 },
+      { name: "Yoga ", timer: new TimerList(), key: 2 },
+      { name: "Gym workout ", timer: new TimerList(), key: 3 }
     ]
+
+    this.sequenceList[0].timer.addTimer(new Timer(hours = 0, minutes = 0, secs = 15, name = "Arm stretch"))
+    this.sequenceList[0].timer.addTimer(new Timer(hours = 0, minutes = 0, secs = 5, name = "Relax"))
+    this.sequenceList[0].timer.addTimer(new Timer(hours = 0, minutes = 0, secs = 15, name = "Leg stretch"))
+
+    this.sequenceList[1].timer.addTimer(new Timer(hours = 0, minutes = 0, secs = 30, name = "Child pose"))
+    this.sequenceList[1].timer.addTimer(new Timer(hours = 0, minutes = 0, secs = 30, name = "Cobra pose"))
+    this.sequenceList[1].timer.addTimer(new Timer(hours = 0, minutes = 0, secs = 60, name = "Child pose"))
+    this.sequenceList[1].timer.addTimer(new Timer(hours = 0, minutes = 0, secs = 30, name = "Downward facing dog"))
+    this.sequenceList[1].timer.addTimer(new Timer(hours = 0, minutes = 0, secs = 30, name = "Standing forward bend"))
+    this.sequenceList[1].timer.addTimer(new Timer(hours = 0, minutes = 0, secs = 45, name = "Bridge pose"))
+
+    this.sequenceList[2].timer.addTimer(new Timer(hours = 0, minutes = 0, secs = 30, name = "Push - Barbell bench press"))
+    this.sequenceList[2].timer.addTimer(new Timer(hours = 0, minutes = 0, secs = 15, name = "Pull - Barbell deadlift"))
+    this.sequenceList[2].timer.addTimer(new Timer(hours = 0, minutes = 0, secs = 20, name = "Legs. Barbell squats"))
+    this.sequenceList[2].timer.addTimer(new Timer(hours = 0, minutes = 0, secs = 10, name = "Push ups"))
   }
 
   updateControl(key, newValue) {
@@ -40,18 +58,34 @@ class Main extends Component {
   }
 
   handleSequenceEdit(item) {
-    console.log("Edit Button Pressed! for seq " + item.key)
+    console.log("Edit Button Pressed! for seq index " + item.index + " key " + item.item.key)
   }
 
-  renderItem = ({ item }) => {return (
-    <View style={{padding: 10, borderStyle: 'solid', borderWidth: 1, margin: 5, borderColor: 'black', display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-      <Text style={{fontSize: 15}}>{item.name}{item.key}</Text>
-      <TouchableOpacity onPress={() => this.handleSequenceEditRef(item)}>
-        <Image style={{height: 25, aspectRatio: 1}} source={require('../assets/edit_icon.png')}/>
-      </TouchableOpacity>
-    </View>)}
-
   render () {
+
+    function renderTimerSeq(item, seqEditHandler) {
+      timerSeq = item.item
+      return (
+        <View style={ timerSeqListStyle.timerSeqItem }>
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+            <AutoSizeText style={{ color: 'black' }} mode={ResizeTextMode.group}>
+              {timerSeq.name}
+            </AutoSizeText>
+            <TouchableOpacity onPress={() => seqEditHandler(item)}>
+              <Image style={{height: 25, aspectRatio: 1}} source={require('../assets/edit_icon.png')}/>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )
+    }
+
+    function renderTimerSeqList(timerSeqList, seqEditHandler) {
+      return (
+        <View style={ timerSeqListStyle.container }>
+          <FlatList data={timerSeqList} renderItem={(item) => renderTimerSeq(item, seqEditHandler)} keyExtractor={item => item.key}/>
+        </View>
+      )
+    }
 
     let mainRender = <View></View>
     if (this.state.addTimer == true) {
@@ -60,22 +94,17 @@ class Main extends Component {
           <AddTimer addTimer={this.addTimerRef} control={this.updateControlRef}/>
         </View>
     } else {
-      mainRender =
-        <View style={{ backgroundColor: 'white', height: '100%'}}>
-          <FlatList data={this.sequenceList} renderItem={this.renderItem} keyExtractor={item => item.key}/>
-        </View>
+      mainRender = renderTimerSeqList(this.sequenceList, this.handleSequenceEditRef)
    }
 
     return (
-      <View style={{ display: 'flex', flexDirection: 'column', height: '100%'}}>
-        <View style={{ height: '10%' }}>
+      <View style={ mainStyle.container }>
+        <View style={ mainStyle.headerBox }>
           <Header control={this.updateControlRef}/>
         </View>
-        <View style={{ height: '100%' }}>
+        <View style={ mainStyle.mainContentBox }>
           { mainRender }
         </View>
-
-
       </View>
     )
   }
